febd835e1c5332b2421fac80e3642848b87c5150
garnieni <deathyran26@gmail.com>
2021-12-19 21:14:48 +0100

    mise en place de commentaire

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -11,12 +11,12 @@ import { TodoItem } from '../todolist.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoItemComponent implements OnInit {
+  // input des info pour la tâche
   @Input() Data!: TodoItem;
-
+  // output update
   @Output() updateEvt: EventEmitter<Partial<TodoItem>> = new EventEmitter<Partial<TodoItem>>();
-
+  // output remove
   @Output() removeEvt: EventEmitter<TodoItem> = new EventEmitter<TodoItem>();
-
   @ViewChild('newTextInput') newTextInput!: ElementRef<HTMLInputElement>;
   // tslint:disable-next-line:variable-name
   private _isEditing = false;
@@ -28,13 +28,15 @@ export class TodoItemComponent implements OnInit {
   ngOnInit(): void {
 
   }
+  // envoie la evenement update
   update(val: any): void {
     this.updateEvt.emit(val);
   }
+  // envoie la evenement remove
   remove(): void {
     this.removeEvt.emit();
   }
-
+  // mise en forme pour l'edition
   setEditing(b: boolean): void {
     this._isEditing = b;
     if (b) {
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -32,33 +32,49 @@ export class TodoListComponent implements OnInit {
   append(label: string): void {
     this.service.append(label);
   }
+  // update un valeur d'un item
   uptdateVal(val: any, item: TodoItem): void {
     this.service.update(val, item);
   }
+  // update all item
   updateAll(val: any, TDL: TodoList): void{
 
     for (let item of TDL.items) {
       this.service.update(val, item);
     }
   }
+  // supprime un item
   supprItem(item: TodoItem): void {
     this.service.remove(item);
   }
+  // fonction pour supprimer tout les élément done
+  SupprAllCheck( TDL: TodoList): void{
+    for (let item of TDL.items) {
+      if (item.isDone) {this.service.remove(item); }
+    }
+  }
+  // fonction pour affichage de tout
   public Tous: FctFilter = (item): boolean => {
     return true;
   }
+  // fonction f qui est remplacer pour le filtrage
   _f: ((e: TodoItem) => boolean) = this.Tous;
+  // fontion qui renvoie true si !isDone
   public fAFaire: FctFilter = (item): boolean => {
     return !item.isDone;
   }
+  // fontion qui renvoie true si isDone
   public fFait: FctFilter = (item: TodoItem): boolean => {
     return item.isDone;
   }
+  // fonction qui renvoie le nombre le nombre de tâche restante
   public getNumberItems = (): number => {
     return this.service.getTotalCount();
   }
+ // fonction retour arrière
   public undo = (): TodolistService =>
     this.service = this.service.undo()
+  // fonction retour avant
   public redo = (): TodolistService =>
     this.service = this.service.redo()
   ngOnInit(): void {
